c2c529d0cd6417d9295e498a88d62afb248c4bf7
Fatih Egriboz <egriboz@gmail.com>
2023-01-24 14:19:52 +0300

    works page

--- pages/index.js
+++ pages/index.js
@@ -12,282 +12,39 @@ export default function Home() {
         <title>Homepage</title>
       </Head>
 
-      <Script type="module" strategy="lazyOnload" src="external.js" />
+
       <Layout>
-        {/* <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-4">
 
-        {photos.map((photo, indexOf) => (
-          <Link
-            // href={`/works/${photo.id}`}
-            scroll={false}
-            key={photo.title}
-            href={{
-              pathname: `/works/${photo.slug}`,
-              query: { indexOf: indexOf },
-            }}
-          >
-            <a>
-              <motion.img
-                layoutId={`${photo.id}`}
-                src={photo.src}
-                alt={photo.title}
-                style={{
-                  width: "100%",
-                  height: "300px",
-                  objectFit: "cover",
-                  objectPosition: "center",
-                }}
-              />
-            </a>
-          </Link>
-        ))}
-      </div> */}
-
-
-        <div className="columns" data-scroll-container="">
-          <div className="column column-reverse">
-
-            {photos.map((photo, indexOf) => (
-              <figure className="column__item" key={photo.title}>
-                <div className="column__item-imgwrap">
-                  <Link
-                    // href={`/works/${photo.id}`}
-                    scroll={false}
-
-                    href={{
-                      pathname: `/works/${photo.slug}`,
-                      query: { indexOf: indexOf },
-                    }}
-                  >
-                    <a>
-                      <motion.img
-                        layoutId={`${photo.id}`}
-                        src={photo.src}
-                        alt={photo.title}
-                        style={{
-                          width: "100%",
-                          height: "300px",
-                          objectFit: "cover",
-                          objectPosition: "center",
-                        }}
-                      />
-                    </a>
-                  </Link>
-                </div>
-              </figure>
-            ))}
-
-
-
-
-
-            {/* <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/1.04213a58.jpg" alt="title"
-
+          {photos.map((photo, indexOf) => (
+            <Link
+              // href={`/works/${photo.id}`}
+              scroll={false}
+              key={photo.title}
+              href={{
+                pathname: `/works/${photo.slug}`,
+                query: { indexOf: indexOf },
+              }}
+            >
+              <a>
+                <motion.img
+                  layoutId={`${photo.id}`}
+                  src={photo.src}
+                  alt={photo.title}
+                  style={{
+                    width: "100%",
+                    height: "300px",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                  }}
                 />
-              </div>
-              <figcaption className="column__item-caption"> <span>Cyber Blue</span> <span>2011</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/2.3ff1fdb6.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Gnostic Will</span> <span>2012</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/3.b606be87.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>French Kiss</span> <span>2013</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/4.24fd614c.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Half Life</span> <span>2014</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/5.d13f5e61.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Love Boat</span> <span>2015</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/6.786c7db4.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Golden Ray</span> <span>2016</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/7.df95fe5c.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Blame Game</span> <span>2017</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/8.e7faf38e.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Lone Dust</span> <span>2018</span> </figcaption>
-            </figure> */}
-
-          </div>
-
-          <div className="column">
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/9.ea63bab4.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Lucky Wood</span> <span>2019</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/10.57de09c7.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Good Earth</span> <span>2020</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/11.ba790930.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Empty Words</span> <span>2021</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/12.c3a8d893.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Nonage Line</span> <span>2009</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/13.3bd52250.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Blue Hell</span> <span>2010</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/14.b7263516.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Cold Blood</span> <span>2011</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/15.55bda21b.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Tulip Heat</span> <span>2012</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/16.eb88393b.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Red Wrath</span> <span>2013</span> </figcaption>
-            </figure>
-          </div>
-          <div className="column column-reverse">
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/17.3450839a.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Bold Human</span> <span>2014</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/18.763d23f6.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Loyal Royal</span> <span>2015</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/19.be25549f.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Lone Cone</span> <span>2016</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/20.d7a9356b.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Dutch Green</span> <span>2017</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/21.4c8813a5.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Valley Hill</span> <span>2018</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/22.ec97ea6e.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Kale Hale</span> <span>2019</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/23.49e8893a.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Fake Cake</span> <span>2020</span> </figcaption>
-            </figure>
-            <figure className="column__item">
-              <div className="column__item-imgwrap">
-                <img src="https://tympanus.net/Development/ColumnScroll/24.057dafba.jpg" alt="title"
-
-                />
-              </div>
-              <figcaption className="column__item-caption"> <span>Book Belly</span> <span>2021</span> </figcaption>
-            </figure>
-
-
-
-
-          </div>
+              </a>
+            </Link>
+          ))}
         </div>
 
-        <div className="warning">⚠️ Unfortunately your browser does not support CSS Scroll-Linked Animations, so this demo won-t work for you. If you-re feeling adventurous use Chromium 95+ with “Experimental Web Platform Features” enabled.</div>
-        <div className="info">⚠️ Your browser does not support CSS Scroll-Linked Animations with <code>@scroll-timeline</code>. To make up for this, <a href="https://github.com/flackr/scroll-timeline" target="_top">a JavaScript polyfill</a> with some <abbr title="Web Animations API">WAAPI</abbr> code is used as a fallback.</div>
+
+
         <footer></footer>
       </Layout>
     </>
--- pages/works/[slug].js
+++ pages/works/[slug].js
@@ -9,13 +9,14 @@ import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 import Layout from "../../components/Layout/works";
 import Image from "next/image";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { photos } from "../../data/photos";
 import { motion } from "framer-motion";
 import { LoremIpsum } from "react-lorem-ipsum";
 import Pagination from "../../components/Pagination";
-
+import Script from "next/script";
 const visible = { opacity: 1, y: 0, transition: { duration: 0.5 } };
 const itemVariants = {
   hidden: { opacity: 0, y: 10 },
@@ -30,12 +31,33 @@ const PhotoDetail = () => {
   const { photoId } = router.query;
   const [photo, setPhoto] = useState();
   const totalPage = photos.length;
-  console.log("photos[photoId]", photoId);
-  console.log("photos::::=====:", photos);
-  console.log("indexOf::::=====:", indexOf);
-  console.log("photos.indexOf(slug)::::", photos.indexOf(slug));
 
-  console.log("slug: : ", slug);
+  /*
+    const { asPath } = router;
+    console.log("asPathasPathasPath: ", asPath);
+    useEffect(() => {
+      const script = document.createElement('script');
+  
+      script.src = "external.js";
+      script.async = true;
+      script.strategy = "lazyOnload"
+      script.type = "module";
+  
+      document.body.appendChild(script);
+  
+      return () => {
+        document.body.removeChild(script);
+      }
+    }, [asPath]);
+  */
+
+
+  // console.log("photos[photoId]", photoId);
+  // console.log("photos::::=====:", photos);
+  // console.log("indexOf::::=====:", indexOf);
+  // console.log("photos.indexOf(slug)::::", photos.indexOf(slug));
+
+  // console.log("slug: : ", slug);
   /* */
   // const currentPost = photos.find((item) => item.id == photoId);
   // const currentPostIndex = photos.findIndex((item) => item.id === photoId);
@@ -53,7 +75,7 @@ const PhotoDetail = () => {
 
   const nextPage = `${Number(indexOf) + 1}`;
   const prevPage = `${Number(indexOf) - 1}`;
-  console.log("prevPage:", prevPage, "nextPage: ", nextPage);
+  // console.log("prevPage:", prevPage, "nextPage: ", nextPage);
   /* */
 
   useEffect(() => {
@@ -97,18 +119,31 @@ const PhotoDetail = () => {
   // console.log("filtered", filtered);
 
   return (
-    <Layout>
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr,1.5fr] min-h-screen">
-        <div className="p-10">
-          <div className="sticky top-10">
-            <div className="flex justify-between mb-8">
-              <Link href="/">
-                <a className="inline-flex items-center rounded-md border border-gray-300 bg-white pl-3 pr-4 py-2 text-sm font-medium text-gray-700 hover:text-white hover:bg-purple-600 hover:border-purple-600 focus:border-purple-200 focus:outline-0 focus:ring-2 focus:ring-purple-300 group">
-                  <ArrowSmallLeftIcon className="w-5 h-5 mr-2 text-gray-300 group-hover:text-purple-200" />
-                  HOME
-                </a>
-              </Link>
-              {/* <a
+    <>
+      <Head>
+        <title>Works Detail</title>
+      </Head>
+
+
+      <Layout>
+
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr,1.5fr] min-h-screen">
+          <div className="p-10">
+            <div className="sticky top-10">
+              <div className="flex justify-between mb-8">
+                <Link href="/">
+                  <a className="inline-flex items-center rounded-md border border-gray-300 bg-white pl-3 pr-4 py-2 text-sm font-medium text-gray-700 hover:text-white hover:bg-purple-600 hover:border-purple-600 focus:border-purple-200 focus:outline-0 focus:ring-2 focus:ring-purple-300 group">
+                    <ArrowSmallLeftIcon className="w-5 h-5 mr-2 text-gray-300 group-hover:text-purple-200" />
+                    HOME
+                  </a>
+                </Link>
+                <Link href="/works">
+                  <a className="inline-flex items-center rounded-md border border-gray-300 bg-white pl-3 pr-4 py-2 text-sm font-medium text-gray-700 hover:text-white hover:bg-purple-600 hover:border-purple-600 focus:border-purple-200 focus:outline-0 focus:ring-2 focus:ring-purple-300 group">
+                    <ArrowSmallLeftIcon className="w-5 h-5 mr-2 text-gray-300 group-hover:text-purple-200" />
+                    WORKS
+                  </a>
+                </Link>
+                {/* <a
                 href={photo.link}
                 target="_blank"
                 rel="noreferrer"
@@ -117,10 +152,10 @@ const PhotoDetail = () => {
                 View Source
                 <ArrowUpRightIcon className="w-4 h-4 text-gray-400 ml-2 group-hover:text-purple-400" />
               </a> */}
-            </div>
-            <div>
-              <hr />
-              {/* <Pagination
+              </div>
+              <div>
+                <hr />
+                {/* <Pagination
               photos={photos}
               photoId={photoId}
               items={photos.length} // 100
@@ -130,70 +165,70 @@ const PhotoDetail = () => {
               style={{ border: "1px solid red" }}
             />
             <hr /> */}
-              {photos.map((item, indexOf) => {
-                return (
-                  <ul key={item.id} className={styles.pagination}>
-                    <li
-                      className={
-                        item.id == currentPage
-                          ? styles.pageItemActive
-                          : styles.pageItem
-                      }
-                    >
-                      <Link
-                        href={{
-                          pathname: `/works/${item.slug}`,
-                          query: { indexOf: indexOf },
-                        }}
+                {photos.map((item, indexOf) => {
+                  return (
+                    <ul key={item.id} className={styles.pagination}>
+                      <li
+                        className={
+                          item.id == currentPage
+                            ? styles.pageItemActive
+                            : styles.pageItem
+                        }
                       >
-                        <a
-                          onClick={() => onPageChange(item.id)}
-                          className={styles.pageLink}
+                        <Link
+                          href={{
+                            pathname: `/works/${item.slug}`,
+                            query: { indexOf: indexOf },
+                          }}
                         >
-                          indexOf:={indexOf}-{item.title}:{indexOf}:
-                          {currentPage}---
-                        </a>
-                      </Link>
-                    </li>
-                  </ul>
-                );
-              })}
-              <hr />
+                          <a
+                            onClick={() => onPageChange(item.id)}
+                            className={styles.pageLink}
+                          >
+                            indexOf:={indexOf}-{item.title}:{indexOf}:
+                            {currentPage}---
+                          </a>
+                        </Link>
+                      </li>
+                    </ul>
+                  );
+                })}
+                <hr />
 
-              <button
-                className={
-                  indexOf <= 0 ? styles.disabledButton : styles.enabledButton
-                }
-                type="button"
-                onClick={() =>
-                  router.push({
-                    pathname: `/works/${getPagePrev[prevPage]}`,
-                    query: { indexOf: prevPage },
-                  })
-                }
-              >
-                PREV
-              </button>
+                <button
+                  className={
+                    indexOf <= 0 ? styles.disabledButton : styles.enabledButton
+                  }
+                  type="button"
+                  onClick={() =>
+                    router.push({
+                      pathname: `/works/${getPagePrev[prevPage]}`,
+                      query: { indexOf: prevPage },
+                    })
+                  }
+                >
+                  PREV
+                </button>
 
-              <button
-                className={
-                  indexOf == totalPage - 1
-                    ? styles.disabledButton
-                    : styles.enabledButton
-                }
-                type="button"
-                onClick={() =>
-                  router.push({
-                    pathname: `/works/${getPageNext[nextPage]}`,
-                    query: { indexOf: nextPage },
-                  })
-                }
-              >
-                NEXT
-              </button>
+                <button
+                  className={
+                    indexOf == totalPage - 1
+                      ? styles.disabledButton
+                      : styles.enabledButton
+                  }
+                  type="button"
+                  onClick={() =>
+                    router.push({
+                      pathname: `/works/${getPageNext[nextPage]}`,
+                      query: { indexOf: nextPage },
+                    })
+                  }
+                >
+                  NEXT
+                </button>
 
-              <hr />
-              {/* 
+                <hr />
+                {/* 
             <div>
               {photos.map((item) => {
                 return <p key={item.id}>
@@ -204,30 +239,42 @@ const PhotoDetail = () => {
               })}
             </div>
             <hr /> */}
-            </div>
-            <h2 className="text-4xl font-bold tracking-tight">{photo.title}</h2>
-            <div className="mt-5 text-sm text-gray-600 space-y-2">
-              <p className="flex items-center">
-                <CameraIcon className="w-4 h-4 mr-2 text-purple-500" />
-                {photo.author}
-              </p>
-              <p className="flex items-center">
-                <MapPinIcon className="w-4 h-4 mr-2 text-purple-500" />
-                {photo.location}
-              </p>
-              <p className="flex items-center">
-                <CalendarDaysIcon className="w-4 h-4 mr-2 text-purple-500" />
-                {photo.date}
-              </p>
-            </div>
-            <div className="mt-5 text-sm text-gray-500 space-y-5">
-              <LoremIpsum p={2} />
+              </div>
+              <h2 className="text-4xl font-bold tracking-tight">{photo.title}</h2>
+              <div className="mt-5 text-sm text-gray-600 space-y-2">
+                <p className="flex items-center">
+                  <CameraIcon className="w-4 h-4 mr-2 text-purple-500" />
+                  {photo.author}
+                </p>
+                <p className="flex items-center">
+                  <MapPinIcon className="w-4 h-4 mr-2 text-purple-500" />
+                  {photo.location}
+                </p>
+                <p className="flex items-center">
+                  <CalendarDaysIcon className="w-4 h-4 mr-2 text-purple-500" />
+                  {photo.date}
+                </p>
+              </div>
+              <div className="mt-5 text-sm text-gray-500 space-y-5">
+                <LoremIpsum p={2} />
+              </div>
             </div>
           </div>
-        </div>
-        {/* <div className="overflow-hidden flex"> */}
-        <section className="bg-slate-200">
-          <motion.div layoutId={`${indexOf}`} className="">
+          {/* <div className="overflow-hidden flex"> */}
+          <section className="bg-slate-200">
+            <motion.div layoutId={`${indexOf}`} className="">
+              <img
+                src={photo.src}
+                alt={photo.title}
+
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  marginBottom: "15px",
+                }}
+              />
+            </motion.div>
+
             <img
               src={photo.src}
               alt={photo.title}
@@ -238,31 +285,20 @@ const PhotoDetail = () => {
                 marginBottom: "15px",
               }}
             />
-          </motion.div>
+            <img
+              src={photo.src}
+              alt={photo.title}
 
-          <img
-            src={photo.src}
-            alt={photo.title}
-
-            style={{
-              width: "100%",
-              height: "auto",
-              marginBottom: "15px",
-            }}
-          />
-          <img
-            src={photo.src}
-            alt={photo.title}
-
-            style={{
-              width: "100%",
-              height: "auto",
-              marginBottom: "15px",
-            }}
-          />
-        </section>
-      </div>
-    </Layout>
+              style={{
+                width: "100%",
+                height: "auto",
+                marginBottom: "15px",
+              }}
+            />
+          </section>
+        </div>
+      </Layout>
+    </>
   );
 };
 export default PhotoDetail;
